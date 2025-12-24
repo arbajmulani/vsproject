40dae5660debc367434d3b6831bc264d1df0b6dc
arbajmulani <arbajmulani732@gmail.com>
2025-12-24 18:42:49 +0530

    Add project files.

---
+++ Example/UnitTest1.cs
@@ -0,0 +1,31 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Example
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+
+
+        [Test]
+        public void Test1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://demoapps.qspiders.com/");
+            driver.Manage().Window.Maximize();
+
+            Console.WriteLine(driver.Title);
+
+            IWebElement tileUITestingConcepts = driver.FindElement(By.XPath("//*[@id='optionsBody']/a[1]/div/main/div[2]"));
+
+            tileUITestingConcepts.Click();
+            driver.Quit();
+
+        }
+    }
+}
